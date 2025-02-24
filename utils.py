24f53edee06f3f68d08369f3e90e6856580b0012
yuuunong <ccbb15379@naver.com>
2025-02-25 04:43:52 +0900

    1

--- utils.py
+++ utils.py
@@ -16,14 +16,14 @@ def make_submit(test_df, features, model):
 
 
 
-def base_model(X, y):
+def base_model(X, y, best_params=None):
     # 데이터 분할 (입력 변수와 목표 변수)
 
     # 데이터 분할 (학습 데이터와 테스트 데이터)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # 모델 학습
-    model = RandomForestClassifier(random_state=42)
+    model = RandomForestClassifier(random_state=42, **best_params)
     model.fit(X_train, y_train)
 
     # 모델 평가
