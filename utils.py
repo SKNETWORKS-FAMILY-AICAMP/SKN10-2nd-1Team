24d62a2d593b0a6ca246b8d3d219d7349091c7be
yuuunong <ccbb15379@naver.com>
2025-02-27 09:11:09 +0900

    auc 0.7733

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
+from sklearn.metrics import accuracy_score, classification_report, roc_auc_score, roc_curve, auc
 from imblearn.over_sampling import SMOTE
 import numpy as np
 import matplotlib.pyplot as plt
@@ -52,7 +52,7 @@ def hpo(X_train, y_train):
 def base_model(X, y):
 
     # 데이터 분할 (학습 데이터와 테스트 데이터)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
     # 불균형 데이터 처리
     smote = SMOTE(random_state=42)
@@ -110,4 +110,19 @@ def preprocessing(df):
     df = pd.concat([df.drop(scale_columns, axis=1), pd.DataFrame(scaler.fit_transform(df[scale_columns]), columns=scale_columns)], axis=1)
 
     return df
+
+def roc_auc_curve_plt(y_test, y_pred):
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure(figsize=(10, 10))
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.4f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc='lower right')
+    plt.show()
 
