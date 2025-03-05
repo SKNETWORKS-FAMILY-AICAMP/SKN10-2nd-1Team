20488006ad76ef8ddfac779b0cc7c3f4777495cf
HIHO999 <tegra9045@gmail.com>
2025-03-05 09:58:48 +0900

    설명추가가

--- module/groq_utils.py
+++ module/groq_utils.py
@@ -3,6 +3,9 @@ from module.analysis_utils import generate_churn_analysis_data, generate_prompt_
 import os
 # Groq API 키 설정
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+# env에 키를 등록해야 함
+# 방법: $env:GROQ_API_KEY= "your_api_key" (PowerShell)
+
 
 # Groq 클라이언트 초기화
 client = Groq(api_key=GROQ_API_KEY)
